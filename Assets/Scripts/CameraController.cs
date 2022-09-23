f14e8ffd5f9c2ea67c52fc4fb6efe1cd9646ff7a
Patrick Rasmussen <swftskllz#gmail.com>
2022-09-23 13:24:42 -0400

    add 3d packages

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] cameraLocations;
+    [SerializeField]
+    private GameObject mainCamera;
+
+    private int currentCameraLocationCount = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            currentCameraLocationCount = currentCameraLocationCount < cameraLocations.Length ? currentCameraLocationCount++ : 0;
+            Debug.Log(currentCameraLocationCount);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            currentCameraLocationCount = currentCameraLocationCount >= 0  ? currentCameraLocationCount-- : cameraLocations.Length-1;
+            Debug.Log(currentCameraLocationCount);
+        }
+
+
+        mainCamera.transform.parent = cameraLocations[currentCameraLocationCount].transform;
+        mainCamera.transform.localPosition = Vector3.zero;
+        mainCamera.transform.rotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum PlayerState 
+    {
+        IDLE,
+        MOVING,
+        RUNNING,
+        SLIDING,
+        JUMPING, 
+        FIGHTING,
+    }
+
+    public PlayerState currentState, prevState;
+    public int Health { get; set; }
+
+    public bool IsGrounded { get; set; }
+
+    public void UpdateState() 
+    {
+        if (prevState != currentState) 
+        {
+            switch (currentState) 
+            {
+                
+            }
+
+            prevState = currentState;
+        }
+    }
+
+   
+
+    public void Move(Vector3 direciton, float speed) 
+    {
+
+    }
+
+}
